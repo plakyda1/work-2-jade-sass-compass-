03785d745153111842128bd95e4bab2653dba4f6
Viktor Plakyda <plakyda1@gmail.com>
2016-02-28 15:12:06 +0200

    Dist Final

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,16 @@
 var gulp = require('gulp'),
     jade = require('gulp-jade'),
-    compass = require('gulp-compass');
-  	//browserSync = require('browser-sync');
+    compass = require('gulp-compass'),
+  	browserSync = require('browser-sync');
 
-  // gulp.task('server', function () {
-  // 	browserSync({
-  // 		port: 9000,
-  // 		server: {
-  // 			baseDir: 'app'
-  // 		}
-  // 	});
-  // });
+  gulp.task('server', function () {
+  	browserSync({
+  		port: 9000,
+  		server: {
+  			baseDir: 'app/dist'
+  		}
+  	});
+  });
 
 gulp.task('jade', function() {
   var YOUR_LOCALS = {};
@@ -36,13 +36,28 @@ gulp.task('watch', function(){
 		gulp.watch('app/jade/**/*.jade',['jade']);
     gulp.watch('app/scss/**/*.scss',['compass']);
 	});
+//Js
+gulp.task('js', function() {
+  gulp.src('app/js/*')
+    .pipe(gulp.dest('app/dist/js/'))
+});
+// Шрифты
+gulp.task('fonts', function() {
+  gulp.src('app/fonts/*')
+    .pipe(gulp.dest('app/dist/fonts/'))
+});
+// Картинки
+gulp.task('images', function () {
+  return gulp.src('app/img/**/*')
+  .pipe(gulp.dest('app/dist/img'));
+});
 
-// gulp.task('watchin', function (){
-// 	gulp.watch([
-//     'app/*',
-// 		'app/dist/*.html',
-// 		'app/dist/css/**/*.css'
-// 		]).on('change', browserSync.reload);
-// });
+gulp.task('watchin', function (){
+	gulp.watch([
+    'app/*',
+		'app/dist/*.html',
+		'app/dist/css/**/*.css'
+		]).on('change', browserSync.reload);
+});
 
-gulp.task('default', ['watch']);
+gulp.task('default', ['server', 'fonts', 'watch', 'images', 'js', 'watchin' ]);
